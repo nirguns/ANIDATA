321203acd40b73f120d11e655cac83593a8943a9
nirguns <nirgunsubedi343@gmail.com>
2022-06-03 20:41:29 +0545

    comment

--- atm.py
+++ atm.py
@@ -2,7 +2,7 @@ import tkinter as tk                # python 3
 from tkinter import font  as tkfont # python 3
 #import Tkinter as tk     # python 2
 #import tkFont as tkfont  # python 2
-
+# this is a comment
 class SampleApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
